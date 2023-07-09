9e4285075b873988852d10bb04b1eb48f6a3f90b
christdev05 <orouchristophe@gmail.com>
2023-07-10 00:43:47 +0200

    update commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import store from './store'
+import router from './router'
+import './index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore } from "vuex"
+
+
+const store = createStore({
+
+    state: {
+        user:{
+            data: {},
+            token: 123
+        }
+    },
+
+    getters: {},
+
+    actions: {},
+
+    mutations: {},
+    
+    modules: {}
+})
+
+export default store;
